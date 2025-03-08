b6c23408e25a94be1a3971ec0081c277e929b43b
Lucas Ignacio <114285355+Luc-ignacio@users.noreply.github.com>
2025-03-08 14:50:13 +1000

    Update current date function

--- src/components/dashboard/top-bar/get-current-date.tsx
+++ src/components/dashboard/top-bar/get-current-date.tsx
@@ -1,39 +1,14 @@
 export const GetCurrentDate = () => {
-  const getFormattedDate = () => {
-    const date = new Date();
+  const date = new Date();
 
-    const options: Intl.DateTimeFormatOptions = {
-      weekday: "long",
-      day: "numeric",
-      month: "long",
-      year: "numeric",
-    };
-
-    const formattedDate = new Intl.DateTimeFormat("en-GB", options).format(
-      date
-    );
-
-    return formattedDate.replace(
-      /\b(\d{1,2})\b/,
-      (day) => `${day}${getOrdinalSuffix(Number(day))}`
-    );
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   };
 
-  const getOrdinalSuffix = (day: number) => {
-    if (day > 3 && day < 21) return "th";
-    switch (day % 10) {
-      case 1:
-        return "st";
-      case 2:
-        return "nd";
-      case 3:
-        return "rd";
-      default:
-        return "th";
-    }
-  };
-
-  const today = getFormattedDate();
+  const today = new Intl.DateTimeFormat("en-GB", options).format(date);
 
   return today;
 };
