77e288c28ad9c0c84737b677b780e0cb9206f8cb
Lucas Ignacio <114285355+Luc-ignacio@users.noreply.github.com>
2025-03-11 11:54:53 +1000

    Update GetCurrentDate component

--- src/components/top-bar/get-current-date.tsx
+++ src/components/top-bar/get-current-date.tsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-
 export const GetCurrentDate = () => {
-  const [today, setToday] = useState("");
-
   const date = new Date();
 
   const options: Intl.DateTimeFormatOptions = {
@@ -12,7 +8,7 @@ export const GetCurrentDate = () => {
     year: "numeric",
   };
 
-  setToday(new Intl.DateTimeFormat("en-GB", options).format(date));
+  const today = new Intl.DateTimeFormat("en-GB", options).format(date);
 
   return today;
 };
